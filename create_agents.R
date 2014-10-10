ad57a399ce058b33728a8f46f0057046a4b5a44a
Matthew Routley <matt@routleynet.org>
2014-10-10 15:23:17 -0400

    Change colours

--- create_agents.R
+++ create_agents.R
@@ -12,7 +12,8 @@ geo_agents <- dplyr::left_join(ct_geo, agents)
 library(ggmap)
 library(mapproj)
 toronto_map <- qmap("queens park,toronto", zoom = 11, maptype = 'terrain')
+#toronto_map <- qmap("dupont and avenue, toronto", zoom = 11, maptype = 'terrain')
 
 toronto_map +
   geom_polygon(aes(x=long, y=lat, group=group, fill=cut_interval(Engagement, n=3)), alpha = 4/6, data=geo_agents) +
-  scale_fill_brewer("Engagement", labels=c("Low", "Medium", "High"))
+  scale_fill_brewer("Engagement", labels=c("Low", "Medium", "High"), palette = "OrRd")
