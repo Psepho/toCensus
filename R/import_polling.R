66218999e8dd0c03589ad2ec1f19714357c9c83e
Matthew Routley <matt@routleynet.org>
2014-10-18 18:07:14 -0400

    Use municipal engagement

--- R/import_polling.R
+++ R/import_polling.R
@@ -7,7 +7,7 @@ import_polling <- function() {
   filename <- "data-raw/Polling data for agents.xlsx"
   sheetname <- "Groupings"
   workbook <- XLConnect::loadWorkbook(filename)
-  polling <- data.frame(XLConnect::readWorksheet(object=workbook, sheetname, startRow = 2, useCachedValues = TRUE))
+  polling <- data.frame(XLConnect::readWorksheet(object=workbook, sheetname, startRow = 2, useCachedValues = TRUE))[,c(1:7,10)]
   names(polling) <- c("gender", "age_range", "region", "income_range", "Tory", "Ford", "Chow", "Engagement")
   polling <- transform(polling, gender = as.factor(gender), income_range = as.factor(income_range), age_range = as.factor(age_range), region = as.factor(region))
   polling <- dplyr::group_by(polling, gender, age_range, income_range, region)
