741072ddc8760ff33862678c25eb5427e84b4a8e
Matthew Routley <matt@routleynet.org>
2014-10-20 12:09:28 -0400

    Add in simulations

--- create_agents.R
+++ create_agents.R
@@ -2,7 +2,15 @@ library(toCensus)
 library(dplyr)
 library(reshape2)
 polling <- import_polling()
-agents <- to_voters(voters, 1000000)
+n <- 100000
+sim <- 10
+agents <- to_voters(voters, n)
+agents$sim <- 1
+for (i in 2:sim) {
+  new_agents <- to_voters(voters, n)
+  new_agents$sim <- i
+  agents <- rbind(agents, new_agents)
+}
 
 regions <- ct_geo %>%
   group_by(Geo_Code, region) %>%
@@ -13,17 +21,24 @@ agents <- dplyr::left_join(agents, regions)
 agents <- dplyr::left_join(agents, polling)
 agents <- agents[!is.na(agents$Engagement),]
 
-candidates <- names(agents)[8:10]
+agents <- agents %>%
+  select(Geo_Code, sim, Tory, Ford, Chow, Engagement)
+
+candidates <- names(agents)[3:5]
 agents$support <- NA
 agents$vote <- NA
 for (i in 1:dim(agents)[1]) {
-  agents$support[i] <- sample(candidates, 1, replace = TRUE, prob = agents[i, 8:10])
-  agents$vote[i] <- ifelse(runif(1) > agents[i, 11], 0, 1)
+  agents$support[i] <- sample(candidates, 1, replace = TRUE, prob = agents[i, 3:5])
+  agents$vote[i] <- ifelse(runif(1) > agents[i, 6], 0, 1)
 }
 
 ct_summary <- agents %>%
-  group_by(Geo_Code, support) %>%
+  group_by(sim, Geo_Code, support) %>%
   transmute(votes = sum(vote))
+ct_summary <- ct_summary %>%
+  group_by(Geo_Code, support) %>%
+  transmute(votes = mean(votes),
+            error = sd(votes))
 
 geo_summary <- dplyr::left_join(ct_geo, ct_summary)
 
