cbef9b4a2fbbc050b65eb45bf8ca3bdabce0aff6
Matthew Routley <matt@routleynet.org>
2014-10-20 17:06:27 -0400

    Drop NAs

--- create_agents.R
+++ create_agents.R
@@ -33,12 +33,16 @@ for (i in 1:dim(agents)[1]) {
 }
 
 ct_summary <- agents %>%
-  group_by(sim, Geo_Code, support) %>%
-  transmute(votes = sum(vote))
+  group_by(sim, Geo_Code, support, add = FALSE) %>%
+  summarise(votes = sum(vote))
 ct_summary <- ct_summary %>%
   group_by(Geo_Code, support) %>%
-  transmute(votes = mean(votes),
-            error = sd(votes))
+  summarise(votes = mean(votes),
+            error = sd(votes, na.rm = TRUE))
+total_votes <- ct_summary %>%
+  group_by(support) %>%
+  summarize(votes = sum(votes))
+prop.table(tapply(total_votes$votes, total_votes[1], sum))
 
 geo_summary <- dplyr::left_join(ct_geo, ct_summary)
 
@@ -47,6 +51,6 @@ library(mapproj)
 toronto_map <- qmap("queens park,toronto", zoom = 11, color = "bw", maptype = 'terrain', extent = 'device', legend = "bottomright", source = "google")
 
 toronto_map +
-  geom_polygon(aes(x=long, y=lat, group=group, fill=cut_interval(votes, n=5)), alpha = 4/6, data=geo_summary) +
+  geom_polygon(aes(x=long, y=lat, group=group, fill=cut_interval(votes, n=5)), alpha = 4/6, data=filter(geo_summary, support != "NA")) +
   scale_fill_brewer("Votes", labels=c("Low", "", "", "", "High"), palette = "OrRd") +
   facet_wrap(~support)
