6c0902253abde1fb4d49cb8be3237414d093871b
Matthew Routley <matt@routleynet.org>
2015-04-27 16:50:43 -0400

    Parse structure

--- data-raw/census_xml.R
+++ data-raw/census_xml.R
@@ -20,3 +20,10 @@ data <- data.frame(matrix(labels, ncol = 3, byrow = TRUE))
 names(data) <- c("geo", "MTNDr", "Sex")
 data$years <- years
 data$values <- values
+
+struct <- xml2::read_xml("data-raw/structure.xml")
+mtndr_num <- xml2::xml_attr(xml2::xml_find_all(struct, "//*/structure:CodeList [@id='CL_MTNDR']/structure:Code", xml2::xml_ns(struct)), "value")
+mtndr_desc <- xml2::xml_text(
+  xml2::xml_find_all(struct, "//*/structure:CodeList [@id='CL_MTNDR']/structure:Code/structure:Description [@xml:lang='en']", xml2::xml_ns(struct))
+  )
+structure <- data.frame(code = mtndr_num, description = mtndr_desc)
