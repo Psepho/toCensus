98b52d3dc3c838453cca05050927e6744db485e9
Matthew Routley <matt@routleynet.org>
2015-05-05 14:30:58 -0400

    Switch to full xml files

--- data-raw/census_xml.R
+++ data-raw/census_xml.R
@@ -1,4 +1,4 @@
-doc2 <- xml2::read_xml("data-raw/census.xml")
+doc2 <- xml2::read_xml("data-raw/98-311-XCB2011019/Generic_98-311-XCB2011019.xml")
 ns <- xml2::xml_ns(doc2)
 to_nodes <- "//d1:GenericData/d1:DataSet/generic:Series/generic:SeriesKey/*[@concept = 'GEO' and @value = '35535']/../.."
 years <- as.integer(xml2::xml_text(xml2::xml_find_all(doc2, stringr::str_c(to_nodes, "/generic:Obs/generic:Time"), ns)))
@@ -9,7 +9,7 @@ names(data) <- c("geo", "MTNDr", "Sex")
 data$years <- years
 data$values <- values
 
-struct <- xml2::read_xml("data-raw/structure.xml")
+struct <- xml2::read_xml("data-raw/98-311-XCB2011019/Structure_98-311-XCB2011019.xml")
 mtndr_num <- xml2::xml_attr(xml2::xml_find_all(struct, "//*/structure:CodeList [@id='CL_MTNDR']/structure:Code", xml2::xml_ns(struct)), "value")
 mtndr_desc <- xml2::xml_text(
   xml2::xml_find_all(struct, "//*/structure:CodeList [@id='CL_MTNDR']/structure:Code/structure:Description [@xml:lang='en']", xml2::xml_ns(struct))
